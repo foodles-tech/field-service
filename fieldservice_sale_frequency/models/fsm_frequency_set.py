51fc76fa7b510d2cf6f8d8a8a97a05d737a5aad6
hparfr <raphael.reverdy@akretion.com>
2021-09-27 17:19:06 +0200

    fixup! [FIX] add migtration script & fix pre-commit

--- fieldservice_sale_frequency/models/fsm_frequency_set.py
+++ fieldservice_sale_frequency/models/fsm_frequency_set.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 Akretion (http:\/\/www.akretion.com)
+# Copyright (C) 2020 Akretion (https://www.akretion.com)
 # @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -8,5 +8,4 @@ from odoo import _, fields, models
 class FSMFrequencySet(models.Model):
     _inherit = "fsm.frequency.set"
 
-    is_abstract = fields.Boolean(oldname="is_abstract")
-    # todo: rename it like
+    is_abstract = fields.Boolean()
