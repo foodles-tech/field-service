90f4b6244250e4ef609c30acdecb12d3f2cf9891
hparfr <raphael.reverdy@akretion.com>
2021-12-30 11:56:24 +0100

    Revert "[IMP] add new val for field_service_tracking ==> generate order or recurring order based on sale line frequency + add fsm_order_type on sale line to type of fsm order for product without fsm order template"
    This reverts commit 1d77a42b0873faafd63ec260dbcfde2e0e201a9e.
    
    Keep fieldservice_tracking drop fsm_type

--- fieldservice_sale_frequency/models/__init__.py
+++ fieldservice_sale_frequency/models/__init__.py
@@ -1,6 +1,4 @@
 from . import (
     fsm_frequency_set,
     sale_order_line,
-    product_template,
-    fsm_recurring_order,
 )
--- fieldservice_sale_frequency/models/sale_order_line.py
+++ fieldservice_sale_frequency/models/sale_order_line.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 Akretion (http:\/\/www.akretion.com)
+# Copyright (C) 2020 Akretion (http:\/\/www.akretion.com)
 # @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -15,31 +15,12 @@ class SaleOrderLine(models.Model):
         help="Frequency of the service",
         domain=[("is_abstract", "=", True)],
     )
-    fsm_order_type_id = fields.Many2one("fsm.order.type", string="Fsm order Type")
-
-    def _field_create_fsm_order_prepare_values(self):
-        self.ensure_one()
-        vals = super()._field_create_fsm_order_prepare_values()
-        if self.fsm_order_type_id:
-            vals["type"] = self.fsm_order_type_id.id
-        return vals
-
-    def _field_create_fsm_recurring_prepare_values(self):
-        self.ensure_one()
-        vals = super()._field_create_fsm_recurring_prepare_values()
-        if self.fsm_order_type_id:
-            vals["fsm_order_type_id"] = self.fsm_order_type_id.id
-        return vals
-
     def _field_service_generation(self):
         """For service lines, create the field service order or requiring order
         depending on  fsm_frequency_set_id. If it already
         exists, it simply links the existing one to the line.
         """
         result = super()._field_service_generation()
-        import ipdb
-
-        ipdb.set_trace()
         for so_line in self.filtered(
             lambda sol: sol.product_id.field_service_tracking
             == "based_on_sale_line_frequency"
